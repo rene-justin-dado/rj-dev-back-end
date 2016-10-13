41fe2ea041663b7ba95ba694bcad4f5aa7df9718
Justin <justindado@yahoo.com>
2016-10-13 16:43:37 +1300

    added router to handle routes

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.port || 3000
 
-const db = require('./lib/database')
+const projects = require('./routes/v1/projects')
 
-app.get('/', (req, res) => {
-  db.getAllProjects()
-    .then(data => res.json({data: data}))
-})
+app.use('/v1/projects', projects)
 
 app.listen (PORT, () => console.log(`Listening on port: ${PORT}`))
