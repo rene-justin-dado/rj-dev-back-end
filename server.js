1256ba40b5e8c724af15b5ae08456c91c3ce56ba
Justin <justindado@yahoo.com>
2016-10-13 16:06:51 +1300

    added example route

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.port || 3000
 
-app.get('/', (req, res) => res.send("Hello"))
+const db = require('./lib/database')
+
+app.get('/', (req, res) => {
+  db.getAllProjects()
+    .then(data => res.json({data: data}))
+})
 
 app.listen (PORT, () => console.log(`Listening on port: ${PORT}`))
