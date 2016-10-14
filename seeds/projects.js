7c67c57d60c7cee3af35652775cacdc337833dec
Justin <justindado@yahoo.com>
2016-10-14 20:07:39 +1300

    reseeded projects with octocat icons

--- seeds/projects.js
+++ seeds/projects.js
@@ -6,17 +6,20 @@ exports.seed = function(knex, Promise) {
         knex('projects').insert({
           name: 'Quote Generator',
           github_link: 'https://github.com/rene-justin-dado/quote-generator',
-          deployment_link: 'https://rene-justin-dado.github.io/quote-generator/'
+          deployment_link: 'https://rene-justin-dado.github.io/quote-generator/',
+          github_icon: 'classic-octocat.png'
         }),
         knex('projects').insert({
           name: 'JS Calculator',
           github_link: 'https://github.com/rene-justin-dado/JS-calculator',
-          deployment_link: 'https://rene-justin-dado.github.io/JS-calculator/'
+          deployment_link: 'https://rene-justin-dado.github.io/JS-calculator/',
+          github_icon: 'coder-octocat'
         }),
         knex('projects').insert({
           name: 'Cobra Thesaurus',
           github_link: 'https://github.com/shsaci/cobra-telegram',
-          deployment_link: ''
+          deployment_link: '',
+          github_icon: 'cyber-octocat'
         })
       ])
     })
