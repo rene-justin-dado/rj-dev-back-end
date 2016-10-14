b7853c87a99440e9378ca91c497b75dad0b3646e
Justin <justindado@yahoo.com>
2016-10-14 20:12:35 +1300

    fixed mistyped links

--- seeds/projects.js
+++ seeds/projects.js
@@ -13,13 +13,13 @@ exports.seed = function(knex, Promise) {
           name: 'JS Calculator',
           github_link: 'https://github.com/rene-justin-dado/JS-calculator',
           deployment_link: 'https://rene-justin-dado.github.io/JS-calculator/',
-          github_icon: 'coder-octocat'
+          github_icon: 'coder-octocat.jpg'
         }),
         knex('projects').insert({
           name: 'Cobra Thesaurus',
           github_link: 'https://github.com/shsaci/cobra-telegram',
           deployment_link: '',
-          github_icon: 'cyber-octocat'
+          github_icon: 'cyber-octocat.gif'
         })
       ])
     })
