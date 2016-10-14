04d2b9c53fa171fb8909c17e51f4acd2283fcf77
Justin <justindado@yahoo.com>
2016-10-14 18:26:40 +1300

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express'),
+      cors = require('cors')
+      app = express(),
+      PORT = process.env.port || 3000,
+      projects = require('./routes/v1/projects')
 
-const app = express()
-const PORT = process.env.port || 3000
-
-const projects = require('./routes/v1/projects')
+app.use(cors())
 
 app.use('/v1/projects', projects)
 
