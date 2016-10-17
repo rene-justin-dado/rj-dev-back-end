619463887176a52d8d18633b9ec61e9eb99d864f
Justin <justindado@yahoo.com>
2016-10-17 16:52:41 +1300

    using postgres on production

--- knexfile.js
+++ knexfile.js
@@ -27,13 +27,8 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'knex_migrations'
     }
