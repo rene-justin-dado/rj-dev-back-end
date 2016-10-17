9bf0dc5c1a5fd66177bd23b436062f285af74ce1
Justin <justindado@yahoo.com>
2016-10-17 17:34:05 +1300

    change port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
       cors = require('cors')
       app = express(),
-      PORT = process.env.port || 3000,
+      PORT = process.env.PORT || 3000,
       projects = require('./routes/v1/projects')
 
 app.use(cors())
