9fcf7805aa53edb80e826ac427c93d7b767e11b5
Justin <justindado@yahoo.com>
2016-10-17 19:04:02 +1300

    updated projects database

--- seeds/projects.js
+++ seeds/projects.js
@@ -17,6 +17,26 @@ exports.seed = function(knex, Promise) {
           name: 'Cobra Thesaurus',
           github_link: 'https://github.com/shsaci/cobra-telegram',
           deployment_link: ''
+        }),
+        knex('projects').insert({
+          name: 'Stupid Questions (front-end stuff; see the readme for details)',
+          github_link: 'https://github.com/nikau-2016/stupid-questions-client',
+          deployment_link: 'TBA'
+        }),
+        knex('projects').insert({
+          name: 'Stupid Questions (front-end stuff; see the readme for details)',
+          github_link: 'https://github.com/nikau-2016/stupid-questions',
+          deployment_link: 's2pid-kweschinz.herokuapp.com'
+        }),
+        knex('projects').insert({
+          name: '#adulting (front-end stuff; see the readme for details)',
+          github_link: 'https://github.com/nikau-2016/lifestack-client.git',
+          deployment_link: 'https://adulting.herokuapp.com'
+        }),
+        knex('projects').insert({
+          name: '#adulting (back-end stuff; see the readme for details)',
+          github_link: 'https://github.com/nikau-2016/lifestack.git',
+          deployment_link: 'https://adulting-server.herokuapp.com/'
         })
       ])
     })
