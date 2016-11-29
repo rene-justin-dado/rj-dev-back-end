5ea2d8b4fbb248d7905d258c2d6b9a81d34554d2
Justin Dado <justindado@yahoo.com>
2016-11-29 22:00:48 +1300

    added blogs route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express'),
       cors = require('cors')
       app = express(),
-      PORT = process.env.PORT || 3000,
-      projects = require('./routes/v1/projects')
+      PORT = process.env.port || 3000,
+      projects = require('./routes/v1/projects'),
+      blogs = require('./routes/v1/blogs')
 
 app.use(cors())
 
 app.use('/v1/projects', projects)
+app.use('/v1/blogs', blogs)
 
 app.listen (PORT, () => console.log(`Listening on port: ${PORT}`))
