c0f1c0fb148bca41f2e2478f4b50a579fe1927da
Justin <justindado@yahoo.com>
2016-12-24 19:47:41 +1300

    back to PORT

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 // const db = require('./postgres')
 const projects = require('../routes/v1/projects')
 const blogs = require('../routes/v1/blogs')
