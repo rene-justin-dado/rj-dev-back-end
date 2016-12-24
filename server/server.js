61fd78e6b52b3c96598af534cca2d66853fdc9a5
Justin <justindado@yahoo.com>
2016-12-24 20:50:42 +1300

    using 3000 when available

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const projects = require('../routes/v1/projects')
 const blogs = require('../routes/v1/blogs')
 
