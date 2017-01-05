05459109eb3da4a97ae8c1f0c8b0c19a829be4e6
Justin <justindado@yahoo.com>
2017-01-06 10:46:38 +1300

    fixed - error in blogs schema

--- migrations/20161203213952_blogs.js
+++ migrations/20161203213952_blogs.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     table.increments('id').primary()
     table.string('category')
     table.string('title')
-    table.json('content')
+    table.string('sub_title')
   })
 };
 
--- seeds/blogs.js
+++ seeds/blogs.js
@@ -9,25 +9,25 @@ exports.seed = function(knex, Promise) {
           id: 99999,
           category: 'EDA culture',
           title: 'C1 - First Reflections',
-          sub-title: "I'm taking the opportunity, on this fine, sunny Friday morning to stay inside and write this, my first ever blog post. These are just a few thoughts I have about EDA so far at the start of phase-0, with the lovely Nikau cohort."
+          sub_title: "I'm taking the opportunity, on this fine, sunny Friday morning to stay inside and write this, my first ever blog post. These are just a few thoughts I have about EDA so far at the start of phase-0, with the lovely Nikau cohort."
         }),
         knex('blogs').insert({
           id: 99998,
           category: 'EDA culture',
           title: 'C1 - Time and Habits',
-          sub-title: "Here's my second post. Hope this looks okay. @.@"
+          sub_title: "Here's my second post. Hope this looks okay. @.@"
         }),
         knex('blogs').insert({
           id: 99997,
           category: 'EDA culture',
           title: 'C2 - Emotional Intelligence',
-          sub-title: "EQ > IQ? It's not really news to me now."
+          sub_title: "EQ > IQ? It's not really news to me now."
         }),
         knex('blogs').insert({
           id: 99996,
           category: 'EDA culture',
           title: 'C3 - The Process of Meditation',
-          sub-title: "Here's one about meditation"
+          sub_title: "Here's one about meditation"
         })
       ])
     })
