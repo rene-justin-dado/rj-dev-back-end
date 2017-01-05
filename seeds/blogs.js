606ff36e332e32aee2ea5eaa537d946f587dde84
Justin <justindado@yahoo.com>
2017-01-06 10:40:42 +1300

    rewrote blogs table

--- seeds/blogs.js
+++ seeds/blogs.js
@@ -9,41 +9,25 @@ exports.seed = function(knex, Promise) {
           id: 99999,
           category: 'EDA culture',
           title: 'C1 - First Reflections',
-          content: {
-            "title": "I'm taking the opportunity, on this fine, sunny Friday morning to stay inside and write this, my first ever blog post. These are just a few thoughts I have about EDA so far at the start of phase-0, with the lovely Nikau cohort.",
-            "questions": ["What's your take on the DBC/EDA (Dev Bootcamp/Enspiral Dev Academy) experience?", "What are your impressions?"],
-            "answers": ["To sum it up very simply, it seems to be a very community-centric type of system, not unlike a good (non-toxic) gaming community. Everyone will have their own skills and experiences to bring to the table and their own goals and motivations to work towards. However, I get the sense that, at least from what I got from my own cohort so far, we're all in it for the same reason at the end of the day. We're here to learn and we can lean on each other when it comes down to building our skills.", "I'm in love with the system already. It actually hits very close to home, I think. As a gamer, having been part of several gaming communities, I've drawn a few comparisons between EDA and the communities I've experienced. While the goal in gaming is mainly to have fun (unless you're <em>that</em> type of person... XD), part of the experience is learning the rules of a game, testing out new things and developing skills that are useful within the game world. EDA, I feel, seems to build a community where fun, experimentation and skill development are simply applied to the real world, rather than a virtual one."]
-          }
+          sub-title: "I'm taking the opportunity, on this fine, sunny Friday morning to stay inside and write this, my first ever blog post. These are just a few thoughts I have about EDA so far at the start of phase-0, with the lovely Nikau cohort."
         }),
         knex('blogs').insert({
           id: 99998,
           category: 'EDA culture',
           title: 'C1 - Time and Habits',
-          content: {
-            "title": "Here's my second post. Hope this looks okay. @.@",
-            "questions": ["Which timebox and habits techniques did you try?"],
-            "answers": ["I've managed to stumble into the Growth Mindset on accident (or maybe not?). I've had success jumping into phase-0 with this in mind and have been playing around with resources and tools ever since I applied. (it's really just treating study like play time) I'm also planning out the future in increments.  The Toggl timer in conjunction with the Pomodoro technique was another one of those techniques that I tried out."]
-          }
+          sub-title: "Here's my second post. Hope this looks okay. @.@"
         }),
         knex('blogs').insert({
           id: 99997,
           category: 'EDA culture',
           title: 'C2 - Emotional Intelligence',
-          content: {
-            "title": "EQ > IQ? It's not really news to me now.",
-            "questions": ["What learnings did you take from the chapter?"],
-            "answers": ["That we really need to rethink how we define intelligence to be inclusive of factors beyond intellect and that the world seems to be getting less monochrome, so to speak.<br>There's also a lot to be said about the value of taking time away from task and objective oriented thinking to just focusing on awareness. To knowing all the intimate little details that your brain can process that normally get filtered out when we work just to accomplish a goal."]
-          }
+          sub-title: "EQ > IQ? It's not really news to me now."
         }),
         knex('blogs').insert({
           id: 99996,
           category: 'EDA culture',
           title: 'C3 - The Process of Meditation',
-          content: {
-            "title": "Here's one about meditation",
-            "questions": ["How did the 'process over product' concept affect the way you tackled the site redesign and rebuild?"],
-            "answers": ["My site looks like a butt at the moment. :/ If I wasn't focusing my attention on the actual process of making the site work, I think I'd get a lot more caught up on random little things; Like the colour doesn't look so great, or that post isn't quite centered. I could have wasted a lot of time that could be spent building the site."]
-          }
+          sub-title: "Here's one about meditation"
         })
       ])
     })
