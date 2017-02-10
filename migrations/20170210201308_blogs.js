4f27c4fe1a9f6a40c41612d164c053505bf735d6
Justin <justindado@yahoo.com>
2017-02-10 21:05:07 +1300

    updated blogs table

--- migrations/20170210201308_blogs.js
+++ migrations/20170210201308_blogs.js
@@ -1,10 +1,10 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('blogs', (table) => {
     table.increments('id').primary()
     table.string('category')
     table.string('title')
     table.string('sub_title')
+    table.string('content')
   })
 };
 
